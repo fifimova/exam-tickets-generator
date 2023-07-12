fcfdeb8f6418310e1433f67141e2423ae83def5e
sasha <fi_fimova@mail.ru>
2023-07-12 17:58:33 +0300

    initial commit

---
+++ src/main/java/skypro/course2/examticketsgenerator/ExamTicketsGeneratorApplication.java
@@ -0,0 +1,13 @@
+package skypro.course2.examticketsgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamTicketsGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamTicketsGeneratorApplication.class, args);
+    }
+
+}
